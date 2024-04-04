4734b1a0cda94d67e19036ee676809b56af36e2f
pshistory <psh_a@naver.com>
2024-04-04 23:09:28 +0900

    init commit

---
+++ src/test/java/org/example/jenkinstest/ControllerTest.java
@@ -0,0 +1,24 @@
+package org.example.jenkinstest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ControllerTest {
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    void test() {
+        ResponseEntity<String> response = testRestTemplate.getForEntity("/", String.class);
+        Assertions.assertEquals(response.getBody(), "야호");
+    }
+}
