cd1a0f5d8da0f14fff5d559d54c13e746f9117d6
pshistory <psh_a@naver.com>
2024-04-05 12:21:49 +0900

    test/ya : 테스트코드에 sout 하나더 추가

--- src/test/java/org/example/jenkinstest/ControllerTest.java
+++ src/test/java/org/example/jenkinstest/ControllerTest.java
@@ -20,6 +20,7 @@ public class ControllerTest {
     void test() {
         ResponseEntity<String> response = testRestTemplate.getForEntity("/", String.class);
         System.out.println("test");
+        System.out.println("test");
         Assertions.assertEquals(response.getBody(), "야호는무슨123123");
     }
 }
