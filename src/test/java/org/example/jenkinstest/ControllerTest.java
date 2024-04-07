978eb831caf6da5804404d61e18c0264b2cbfb8a
pshistory <psh_a@naver.com>
2024-04-07 14:56:52 +0900

    cp test

--- src/test/java/org/example/jenkinstest/ControllerTest.java
+++ src/test/java/org/example/jenkinstest/ControllerTest.java
@@ -19,7 +19,7 @@ public class ControllerTest {
     @Test
     void test() {
         ResponseEntity<String> response = testRestTemplate.getForEntity("/", String.class);
-        System.out.println("tesad1st");
+        System.out.println("tesad1stㅁ");
         Assertions.assertEquals(response.getBody(), "야호는123무11111111111슨123123");
     }
 }
