53364d2bac51ed81b42b70537dd9954cb34419f6
pshistory <psh_a@naver.com>
2024-04-14 01:48:39 +0900

    현재 브랜치 확인로직 추가

--- src/main/java/org/example/jenkinstest/Controller.java
+++ src/main/java/org/example/jenkinstest/Controller.java
@@ -9,7 +9,7 @@ public class Controller {
 
     @GetMapping
     public String index(){
-        System.out.println("작업브랜치에서 메인브랜치로 pr 테스트");
+        System.out.println("github.hand_ref 테스트");
         return "야호123";
     }
 }
